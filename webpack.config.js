23726dbb837b3de7599cada93f1156f7c7654a71
adai-wwl <499511987@qq.com>
2017-10-12 19:40:29 +0800

    change to gitee.

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,6 @@ module.exports = {
             comments: false,
             beautify: false
         }),
-        new webpack.BannerPlugin('https://github.com/w-wl/')
+        new webpack.BannerPlugin('https://gitee.com/w-wl/')
     ]
 };
